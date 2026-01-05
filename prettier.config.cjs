3ce47890488af09bfc2fbe54c9d8c836175e45e1
Vitaliy <VitaliyThupin@gmail.com>
2026-01-05 12:22:30 +0200

    init(chore): initialize kedr-web nx workspace"

---
+++ prettier.config.cjs
@@ -0,0 +1,19 @@
+﻿/** @type {import("prettier").Config} */
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+
+  overrides: [
+    {
+      files: '*.html',
+      options: { parser: 'angular' },
+    },
+  ],
+};
