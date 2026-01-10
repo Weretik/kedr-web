d7aefec414721e8d6bf9c4675b6eb11ce0802e9c
Vitaliy <VitaliyThupin@gmail.com>
2026-01-10 14:54:57 +0200

    feat(home): integrate new sections into home page
    - Added `partners-brands`, `company-stats`, and `faq` sections for improved content and UX
    - Enhanced modularity by updating component imports for streamlined integration

--- libs/storefront/feature/home/src/lib/page/home-page/home-page.ts
+++ libs/storefront/feature/home/src/lib/page/home-page/home-page.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { CompanyStats } from '../../sections/company-stats/company-stats';
+import { Faq } from '../../sections/faq/faq';
 import { MainHero } from '../../sections/main-hero/main-hero';
+import { PartnersBrands } from '../../sections/partners-brands/partners-brands';
 
 @Component({
   selector: 'lib-home-page',
-  imports: [MainHero],
+  imports: [MainHero, PartnersBrands, CompanyStats, Faq],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
