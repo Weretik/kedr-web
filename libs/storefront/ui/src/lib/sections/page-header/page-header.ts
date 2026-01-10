dd1a9d715a49ab9ac65b22217adefaa31087be8a
Vitaliy <VitaliyThupin@gmail.com>
2026-01-10 14:54:48 +0200

    feat(page-header): update styles and add breadcrumb customization
    - Updated background and spacing styles for improved design consistency
    - Introduced dynamic breadcrumb theming with dark/light mode support
    - Injected `ThemeService` and added computed property for breadcrumb styling

--- libs/storefront/ui/src/lib/sections/page-header/page-header.ts
+++ libs/storefront/ui/src/lib/sections/page-header/page-header.ts
@@ -1,4 +1,5 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
+import { ThemeService } from '@shared/ui';
 import { MenuItem } from 'primeng/api';
 import { Breadcrumb } from 'primeng/breadcrumb';
 import { IconField } from 'primeng/iconfield';
@@ -14,7 +15,26 @@ import { PageHeaderConfig } from './page-header.config';
   styleUrl: './page-header.css',
 })
 export class PageHeader {
-  config = input.required<PageHeaderConfig>();
+  public themeService = inject(ThemeService);
+  public config = input.required<PageHeaderConfig>();
 
   home: MenuItem = { icon: 'pi pi-home', routerLink: '/' };
+
+  public readonly breadcrumbPt = computed(() => ({
+    root: {
+      style: {
+        paddingLeft: '0',
+        paddingRight: '0',
+        '--p-breadcrumb-background': this.themeService.isDark()
+          ? 'var(--p-surface-900)'
+          : 'var(--p-neutral-0)',
+      },
+    },
+    list: {
+      style: {
+        paddingLeft: '0',
+        marginLeft: '0',
+      },
+    },
+  }));
 }
