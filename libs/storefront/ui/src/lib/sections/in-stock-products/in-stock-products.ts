74ff42fc16af67e93b3d965365ebe56c6128b2fd
Vitaliy <VitaliyThupin@gmail.com>
2026-01-11 21:46:10 +0200

    feat(storefront): add in-stock-products section
    - Implemented a new `in-stock-products` section showcasing available products
    - Added responsive layouts with dynamic content rendering for better UX
    - Enhanced modularity by integrating reusable components and styles

---
+++ libs/storefront/ui/src/lib/sections/in-stock-products/in-stock-products.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { ButtonDirective, ButtonLabel } from 'primeng/button';
+
+interface Products {
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+  buttons: ProductButton[];
+}
+interface ProductButton {
+  label: string;
+  routeLink?: string;
+}
+
+@Component({
+  selector: 'lib-in-stock-products',
+  imports: [ButtonDirective, ButtonLabel],
+  templateUrl: './in-stock-products.html',
+  styleUrl: './in-stock-products.css',
+})
+export class InStockProducts {
+  public productList: Products[] = [
+    {
+      name: 'Golden Nectar Elixir',
+      description: 'Premium Green Nectar, 750ml',
+      price: 250.5,
+      image:
+        'https://fqjltiegiezfetthbags.supabase.co/storage/v1/object/public/block.images/blocks/gridlist/grid-centered.jpg',
+      buttons: [{ label: 'Add to Cart' }, { label: 'Buy Now' }],
+    },
+    {
+      name: 'Ferrari Replica',
+      description: 'Collectible Model Car',
+      price: 350.5,
+      image:
+        'https://fqjltiegiezfetthbags.supabase.co/storage/v1/object/public/block.images/blocks/gridlist/grid-centered-1.jpg',
+      buttons: [{ label: 'Add to Cart' }, { label: 'Buy Now' }],
+    },
+    {
+      name: 'Game Boy Color',
+      description: 'Classic Handheld Console',
+      price: 450.5,
+      image:
+        'https://fqjltiegiezfetthbags.supabase.co/storage/v1/object/public/block.images/blocks/gridlist/grid-centered-2.jpg',
+      buttons: [{ label: 'Add to Cart' }, { label: 'Buy Now' }],
+    },
+  ];
+}
