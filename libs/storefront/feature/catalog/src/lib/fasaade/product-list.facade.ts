ba2ae14698e52d366001945e91dd3d072cc66948
Vitaliy <VitaliyThupin@gmail.com>
2026-01-22 02:03:27 +0200

    feat(storefront): add product list facade and page scaffolding
    - Introduced `ProductListFacade` for managing product list queries, state, and API calls
    - Added initial product list page component with basic rendering and styling
    - Enabled linked query params for filters, sorting, pagination, and search functionality

---
+++ libs/storefront/feature/catalog/src/lib/fasaade/product-list.facade.ts
@@ -0,0 +1,112 @@
+﻿import { Injectable, computed, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import {
+  CatalogApiService,
+  type GetProductListQuery,
+  type ProductListRowDto,
+} from '@storefront/data-access';
+import {
+  mapProductListQueryToApi,
+  type ProductListQuery,
+  ProductListSortUi,
+} from '@storefront/util';
+import { linkedQueryParam } from 'ngxtension/linked-query-param';
+
+import type { PagedResult } from '@shared/data-access';
+
+@Injectable({ providedIn: 'root' })
+export class ProductListFacade {
+  private catalogApi = inject(CatalogApiService);
+
+  readonly search = linkedQueryParam('search');
+  readonly page = linkedQueryParam('page', { defaultValue: '1' });
+  readonly pageSize = linkedQueryParam('pageSize', { defaultValue: '20' });
+  readonly sort = linkedQueryParam('sort', { defaultValue: 'name-asc' });
+  readonly categoryId = linkedQueryParam('categoryId');
+
+  readonly inStock = linkedQueryParam('inStock');
+  readonly isSale = linkedQueryParam('isSale');
+  readonly isNew = linkedQueryParam('isNew');
+
+  readonly priceTypeIdRetail = '10';
+  readonly priceFrom = linkedQueryParam('priceFrom');
+  readonly priceTo = linkedQueryParam('priceTo');
+
+  readonly query = computed<ProductListQuery>(() => ({
+    search: this.search() ?? undefined,
+    categoryId: this.categoryId() ?? undefined,
+
+    inStock: (this.inStock() as '1' | '0' | null) ?? undefined,
+    isSale: (this.isSale() as '1' | '0' | null) ?? undefined,
+    isNew: (this.isNew() as '1' | '0' | null) ?? undefined,
+
+    priceTypeId: this.priceTypeIdRetail,
+    priceFrom: this.priceFrom() ?? undefined,
+    priceTo: this.priceTo() ?? undefined,
+
+    sort: (this.sort() ?? 'name-asc') as ProductListSortUi,
+
+    page: this.page() ?? '1',
+    pageSize: this.pageSize() ?? '20',
+  }));
+
+  readonly apiQuery = computed<GetProductListQuery>(() =>
+    mapProductListQueryToApi(this.query()),
+  );
+
+  readonly productsResource = rxResource<
+    PagedResult<ProductListRowDto>,
+    GetProductListQuery
+  >({
+    params: () => this.apiQuery(),
+    stream: ({ params }) => this.catalogApi.getProductList(params),
+  });
+
+  // =================
+  // UI actions (methods)
+  // =================
+  public setPage(page: number) {
+    this.page.set(String(page));
+  }
+  private setDefaultPage() {
+    this.page.set('1');
+  }
+
+  public setSearch(value: string) {
+    const currentValue = this.search();
+    const nextValue = value?.trim();
+
+    if (currentValue === nextValue) return;
+
+    this.search.set(nextValue ? nextValue : null);
+    this.setDefaultPage();
+  }
+
+  setSort(sort: ProductListSortUi) {
+    this.sort.set(sort);
+    this.setDefaultPage();
+  }
+
+  toggleInStock() {
+    const currentStock = this.inStock();
+    const nextStock = currentStock === '1' ? '0' : '1';
+    this.inStock.set(nextStock);
+    this.setDefaultPage();
+  }
+
+  clearFilters() {
+    this.search.set(null);
+    this.categoryId.set(null);
+
+    this.inStock.set(null);
+    this.isSale.set(null);
+    this.isNew.set(null);
+
+    this.priceFrom.set(null);
+    this.priceTo.set(null);
+
+    this.sort.set('name-asc');
+    this.page.set('1');
+    this.pageSize.set('20');
+  }
+}
