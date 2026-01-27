422fa9e7daeee089760362b5857a305ef5228f51
Vitaliy <VitaliyThupin@gmail.com>
2026-01-27 16:21:59 +0200

    feat(storefront): enhance product list with layout toggle and improved templates
    - Added list and grid layout toggle using `SelectButton` in `ProductList`
    - Integrated `DataView` for flexible product display options
    - Improved `InStockProducts` and `FiltersBar` templates for better readability and maintainability
    - Enhanced product list UI with consistent styling and additional tags for product state

--- libs/storefront/feature/catalog/src/lib/sections/product-list/product-list.ts
+++ libs/storefront/feature/catalog/src/lib/sections/product-list/product-list.ts
@@ -1,14 +1,22 @@
-import { Component, computed, inject } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, inject, computed } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonDirective } from 'primeng/button';
+import { DataView } from 'primeng/dataview';
+import { SelectButton } from 'primeng/selectbutton';
+import { Tag } from 'primeng/tag';
 
 import { ProductListFacade } from '../../pages/product-list/product-list.facade';
 
 @Component({
   selector: 'lib-product-list',
-  imports: [],
+  imports: [DataView, SelectButton, NgClass, Tag, ButtonDirective, FormsModule],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
 export class ProductList {
+  layout: 'list' | 'grid' = 'list';
+  options: string[] = ['list', 'grid'];
   readonly productListFacade = inject(ProductListFacade);
   readonly productsResource = this.productListFacade.productsResource;
 
