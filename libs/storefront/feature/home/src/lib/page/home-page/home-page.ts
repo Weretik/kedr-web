3c47339df007877857df41019a590d627097274a
Vitaliy <VitaliyThupin@gmail.com>
2026-03-06 15:56:01 +0200

    refactor(home): remove unused imports and sections from home page
    - Cleaned up obsolete component imports to streamline codebase
    - Removed unused sections from the home page to reduce clutter and improve maintainability

--- libs/storefront/feature/home/src/lib/page/home-page/home-page.ts
+++ libs/storefront/feature/home/src/lib/page/home-page/home-page.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
-import { PartnersBrands } from '@storefront/ui';
-
-import { CompanyFeatures } from '../../sections/company-features/company-features';
-import { CompanyStats } from '../../sections/company-stats/company-stats';
-import { Faq } from '../../sections/faq/faq';
-import { MainHero } from '../../sections/main-hero/main-hero';
 
 @Component({
   selector: 'lib-home-page',
-  imports: [MainHero, CompanyStats, Faq, CompanyFeatures, PartnersBrands],
+  imports: [],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
