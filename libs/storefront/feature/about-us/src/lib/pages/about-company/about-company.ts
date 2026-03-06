75512cfb885ae4ac5c32ebe07239064aabe8f786
Vitaliy <VitaliyThupin@gmail.com>
2026-03-06 15:55:27 +0200

    feat(about-us): enhance about company page with new sections
    - Added MainHero, CompanyFeatures, CompanyStats, PartnersBrands, and Faq sections
    - Improved modularity and future scalability of the about company page

--- libs/storefront/feature/about-us/src/lib/pages/about-company/about-company.ts
+++ libs/storefront/feature/about-us/src/lib/pages/about-company/about-company.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-import { PageHeader, PageHeaderConfig } from '@storefront/ui';
+import { PageHeaderConfig, PartnersBrands } from '@storefront/ui';
+
+import { CompanyFeatures } from '../../sections/company-features/company-features';
+import { CompanyStats } from '../../sections/company-stats/company-stats';
+import { Faq } from '../../sections/faq/faq';
+import { MainHero } from '../../sections/main-hero/main-hero';
 
 @Component({
   selector: 'lib-about-company',
-  imports: [PageHeader],
+  imports: [MainHero, CompanyFeatures, CompanyStats, PartnersBrands, Faq],
   templateUrl: './about-company.html',
   styleUrl: './about-company.css',
 })
