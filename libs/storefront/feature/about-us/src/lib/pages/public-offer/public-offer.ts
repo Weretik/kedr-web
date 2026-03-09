58457a7323976f6f331d83035a3d7afb9106ae92
Vitaliy <VitaliyThupin@gmail.com>
2026-03-09 20:10:26 +0200

    feat(about-us): add public offer page
    - Created a detailed Public Offer page with legal and usage information
    - Extended the About Us section to include public offer compliance content

---
+++ libs/storefront/feature/about-us/src/lib/pages/public-offer/public-offer.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-public-offer',
+  imports: [],
+  templateUrl: './public-offer.html',
+  styleUrl: './public-offer.css',
+})
+export class PublicOffer {}
