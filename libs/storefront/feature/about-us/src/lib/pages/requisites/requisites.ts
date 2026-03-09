6defe325274b9a3aeb18e3a34200b378ee250aee
Vitaliy <VitaliyThupin@gmail.com>
2026-03-09 20:10:39 +0200

    feat(about-us): add requisites page
    - Created a detailed requisites page with official company details
    - Enhanced About Us section with additional transparency and informational content

---
+++ libs/storefront/feature/about-us/src/lib/pages/requisites/requisites.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-requisites',
+  imports: [],
+  templateUrl: './requisites.html',
+  styleUrl: './requisites.css',
+})
+export class Requisites {}
