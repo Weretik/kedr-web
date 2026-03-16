4e99c936462c0eedc516c4562154a867b78c01f9
Vitaliy <VitaliyThupin@gmail.com>
2026-03-16 19:09:39 +0200

    feat(catalog): export catalog category slugs constants
    - Added export for catalog category slugs constants to improve reusability
    - Ensured better encapsulation and maintainability for category-related configurations

--- libs/storefront/data-access/src/lib/catalog/index.ts
+++ libs/storefront/data-access/src/lib/catalog/index.ts
@@ -2,3 +2,4 @@ export * from './state/product/product.facade';
 export * from './state/product-list/product-list.facade';
 export * from './mappers/product-list-query.mapper';
 export * from './models/product-list-query.model';
+export * from './config/catalog-category-slugs.constants';
