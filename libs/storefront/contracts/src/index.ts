87efc9108a626061868b8fa897d70b2837cd9236
Vitaliy <VitaliyThupin@gmail.com>
2026-03-16 14:34:00 +0200

    refactor(contracts): reorganize and rename models and DTOs
    - Moved models and DTOs to appropriate locations for better logical grouping
    - Renamed files to reflect their purpose more clearly (e.g., `dto` to `result`)
    - Updated exports to align with the new structure for improved module consistency

--- libs/storefront/contracts/src/index.ts
+++ libs/storefront/contracts/src/index.ts
@@ -1,10 +1,7 @@
-export * from './lib/cart/cart.types';
-
 export * from './lib/catalog/get-product-list.query';
-export * from './lib/catalog/product-by-slug.dto';
-export * from './lib/catalog/product-list-query.model';
-export * from './lib/catalog/product-list-row.dto';
+export * from './lib/catalog/product-list.result';
+export * from './lib/catalog/product-by-slug.result';
 export * from './lib/catalog/product-sort.enum';
 
-export * from './lib/checkout/orders.responses';
-export * from './lib/checkout/orders.requests';
+export * from './lib/checkout/orders.command';
+export * from './lib/checkout/orders.result';
