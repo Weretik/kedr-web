c691e8c8707e9a6cbe7d0a38a826e95276e144f6
Vitaliy <VitaliyThupin@gmail.com>
2026-03-16 14:54:47 +0200

    refactor(storefront): move header and footer components to shell library
    - Reorganized header and footer components from UI library to shell library
    - Improved logical structure by grouping shared layout components in the shell module
    - Simplified imports and ensured consistency across modules

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,5 +1,16 @@
-﻿/** @type {import('lint-staged').Config} */
+const path = require('path');
+
+const toRelative = (files) =>
+  files.map((file) => path.relative(process.cwd(), file));
+
+/** @type {import('lint-staged').Config} */
 module.exports = {
-  '*.{ts,js,html,css,scss,json,md}': ['npx prettier --write'],
-  '*.{ts,js}': ['npx eslint --fix']
+  '*.{ts,js,html,css,scss,json,md}': (files) => {
+    const relativeFiles = toRelative(files);
+    return `npx prettier --write ${relativeFiles.join(' ')}`;
+  },
+  '*.{ts,js}': (files) => {
+    const relativeFiles = toRelative(files);
+    return `npx nx affected -t lint --files ${relativeFiles.join(',')}`;
+  },
 };
