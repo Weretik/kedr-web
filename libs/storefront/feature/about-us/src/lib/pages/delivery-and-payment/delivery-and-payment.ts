4dee9d4093ac22c645eea743bc866811167c0390
Vitaliy <VitaliyThupin@gmail.com>
2026-03-21 02:03:49 +0200

    feat(storefront): redesign delivery and payment page with simplified layout
    - Updated page structure for better readability and user experience
    - Refactored content to highlight key delivery, payment, and order processes
    - Improved maintainability with centralized and reusable content styles

--- libs/storefront/feature/about-us/src/lib/pages/delivery-and-payment/delivery-and-payment.ts
+++ libs/storefront/feature/about-us/src/lib/pages/delivery-and-payment/delivery-and-payment.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { PageHeader, PageHeaderConfig } from '@storefront/ui';
 
 @Component({
   selector: 'lib-delivery-and-payment',
-  imports: [PageHeader],
+  imports: [PageHeader, RouterLink],
   templateUrl: './delivery-and-payment.html',
   styleUrl: './delivery-and-payment.css',
 })
